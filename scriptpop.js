7cdaea612be13dfcdf49e3b28fb65c0ae6c2c90a
the-photographrr <arunkarooth6@gmail.com>
2020-12-17 13:01:25 +0530

    commit

--- scriptpop.js
+++ scriptpop.js
@@ -1,9 +1,9 @@
 $( document ).ready(function() {
-          // load the overlay
-      $('#myModal').modal({show:true});
-      
-      var year = 1000*60*60*24*365;
-      var expires = new Date((new Date()).valueOf() + year);
-      document.cookie = "expires=" + expires.toUTCString();
+  // load the overlay
+  $('#myModal').modal({show:true});
   
-  }); 
+  var year = 1000*60*60*24*365;
+  var expires = new Date((new Date()).valueOf() + year);
+  document.cookie = "expires=" + expires.toUTCString();
+
+}); 
